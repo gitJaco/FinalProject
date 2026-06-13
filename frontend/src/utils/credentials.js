3762a8c7bfd2c4441e272ed73e73edbe0a49c3ea
Jacobo <jacopeth88@gmail.com>
2026-06-12 22:09:41 -0600

    Credentials

--- frontend/src/utils/credentials.js
+++ frontend/src/utils/credentials.js
@@ -1,6 +1,6 @@
 function credentials() {
     return (
-        { clientId: 'f57b2400558344939bb69f4d5b0573a9', clientSecret: 'ced9b61ec0984d16ae4113148672e313'}
+        { clientId: 'cae880058b1040eabe5f4841d03e50d9', clientSecret: '82c5477385c44a4c86a962efc80175dd'}
     )
 }
 
